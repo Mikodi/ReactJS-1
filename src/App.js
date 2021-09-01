22bc22279267bdbf801ca0d32e918c4d0223f2ec
Maria <mikodi00@mail.ru>
2021-09-01 22:23:49 +0300

    hw-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  return (
+    <div>
+      <Message {...props} text="Glad to see you on the page"></Message>
+    </div>
+  )
+}
+
+function Message(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="Message-text">Hello, {props.name}</h2>
+        <h3 className="Message-text">{props.text}</h3>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
